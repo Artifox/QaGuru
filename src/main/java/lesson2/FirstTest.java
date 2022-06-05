e75e3ec7613fbdde111dde5247270c6deb475d7d
Artsiom Koptsiukh <artsiom.koptsiukh@customertimes.com>
2022-06-05 09:48:55 +0400

    fix @Test

--- src/main/java/lesson2/FirstTest.java
+++ src/main/java/lesson2/FirstTest.java
@@ -1,10 +1,10 @@
 package lesson2;
 
-import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.Test;
 
 public class FirstTest {
 
-    @Test
+    //@Test
     public void firstTest() {
         System.out.println("Conflict here!!!");
     }
